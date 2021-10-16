d8ccbf693aa3ebd596147fef68b1be0bfeef1877
Tamera <tlbodrick@gmail.com>
2021-10-15 21:24:13 -0400

    added theme toggle functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// calculator functionality
+
 let button = document.querySelectorAll('button');
 let inputScreen = document.querySelector('#screen');
 
@@ -31,4 +33,97 @@ button.forEach(btn => {
 
 
 
+
+let radio = document.querySelectorAll('.radio');
+
+for (let btn of radio) {
+    btn.addEventListener('change', function () {
+        if (btn.checked) {
+
+            if (btn.id === 'radio2') {
+
+
+                // add v2 classlist
+                document.body.classList.add('v2');
+                document.querySelector('#screen').classList.add('v2');
+                document.querySelector('#keypad').classList.add('v2');
+                document.querySelector('.equal').classList.add('v2');
+                document.querySelector('.toggle').classList.add('v2');
+                document.querySelector('.ball').classList.add('v2');
+
+                let allBalls = document.querySelectorAll('.ball');
+
+                for (let i = 0; i < allBalls.length; i++) {
+                    allBalls[i].classList.add('v2');
+                }
+
+
+                let allKeys = document.querySelectorAll('.key');
+
+                for (let i = 0; i < allKeys.length; i++) {
+                    allKeys[i].classList.add('v2');
+                }
+
+                let allAccKeys = document.querySelectorAll('.acc-key');
+
+                for (let i = 0; i < allAccKeys.length; i++) {
+                    allAccKeys[i].classList.add('v2');
+                }
+
+                //remove additional classlists
+                let v3 = document.querySelectorAll('.v3');
+                for (let i = 0; i < v3.length; i++) {
+                    v3[i].classList.remove('v3');
+                }
+
+
+            } else if (btn.id === 'radio3') {
+
+                //add v3 classlist
+                document.body.classList.add('v3');
+                document.querySelector('#screen').classList.add('v3');
+                document.querySelector('#keypad').classList.add('v3');
+                document.querySelector('.equal').classList.add('v3');
+                document.querySelector('.toggle').classList.add('v3');
+
+
+                let allBalls = document.querySelectorAll('.ball');
+                for (let i = 0; i < allBalls.length; i++) {
+                    allBalls[i].classList.add('v3');
+                }
+
+                let allKeys = document.querySelectorAll('.key');
+
+                for (let i = 0; i < allKeys.length; i++) {
+                    allKeys[i].classList.add('v3');
+                }
+
+                let allAccKeys = document.querySelectorAll('.acc-key');
+
+                for (let i = 0; i < allAccKeys.length; i++) {
+                    allAccKeys[i].classList.add('v3');
+                }
+
+                //remove additional classlists
+                let v2 = document.querySelectorAll('.v2');
+                for (let i = 0; i < v2.length; i++) {
+                    v2[i].classList.remove('v2');
+                }
+
+            } else {
+                //remove additional classlists
+                let v2 = document.querySelectorAll('.v2');
+                for (let i = 0; i < v2.length; i++) {
+                    v2[i].classList.remove('v2');
+                }
+
+                let v3 = document.querySelectorAll('.v3');
+                for (let i = 0; i < v3.length; i++) {
+                    v3[i].classList.remove('v3');
+                }
+
+            }
+        }
+    })
+}
 
